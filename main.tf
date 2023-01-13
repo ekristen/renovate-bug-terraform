e129a8a2da1b333eb59dd86b775f49c3ed9e4dea
Erik Kristensen <erik@erikkristensen.com>
2023-01-13 12:15:54 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,15 @@ module "ssh_key_pair" {
   private_key_extension = ".pem"
   public_key_extension  = ".pub"
 }
+
+module "egress_subnets" {
+  source            = "git::https://github.com/cloudposse/terraform-aws-named-subnets.git?ref=0.9.2"
+  subnet_names      = ["egress1"]
+  max_subnets       = 1
+  vpc_id            = "vpc-blah"
+  cidr_block        = "10.0.0.0/16"
+  type              = "public"
+  igw_id            = "igw-blah"
+  availability_zone = "us-east-2"
+  nat_enabled       = true
+}
