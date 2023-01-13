1efebe214128dfbbeaf9caed6838323c787b4a97
Erik Kristensen <erik@erikkristensen.com>
2023-01-13 12:14:20 -0700

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.74.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.1.0"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.1.0"
+    }
+
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.2.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 3.1.0"
+    }
+  }
+
+  required_version = "< 1.2"
+}
